348e2d9dd03164e40c6a654b56efdf0f4fdf3d5b
danielgrigore <danielgrigore@gmail.com>
2015-12-14 18:37:11 +0200

    Updated ProxyFactoryBean to read proxy info from environment properties

--- cosmo-core/src/main/java/org/unitedinternet/cosmo/ext/ProxyFactoryBean.java
+++ cosmo-core/src/main/java/org/unitedinternet/cosmo/ext/ProxyFactoryBean.java
@@ -4,23 +4,43 @@ import java.net.InetSocketAddress;
 import java.net.Proxy;
 import java.net.Proxy.Type;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.FactoryBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.core.env.Environment;
 
 /**
- * TODO Configure it properly.
+ * <code>Factory</code> that creates a <code>java.net.Proxy</code> that is used when making requests for getting
+ * external content.
  * 
  * @author daniel grigore
  *
  */
-public class ProxyFactoryBean implements FactoryBean<Proxy> {
+public class ProxyFactoryBean implements FactoryBean<Proxy>, ApplicationContextAware {
+
+    private static final Log LOG = LogFactory.getLog(ProxyFactoryBean.class);
+    private static final String PROP_PROXY_HOST = "external.proxy.host";
+    private static final String PROP_PROXY_PORT = "external.proxy.port";
+
+    private Environment env;
+
+    public ProxyFactoryBean() {
+        super();
+    }
 
     @Override
     public Proxy getObject() throws Exception {
-        boolean noproxy = false;
-        if (noproxy) {
-            return Proxy.NO_PROXY;
+        String proxyHost = this.env.getProperty(PROP_PROXY_HOST);
+        String proxyPort = this.env.getProperty(PROP_PROXY_PORT);
+        if (proxyHost != null && !proxyHost.trim().isEmpty() && proxyPort != null && !proxyPort.trim().isEmpty()) {
+            LOG.info("NET Setting proxy host: " + proxyHost + " and port: " + proxyPort
+                    + " for getting external content.");
+            return new Proxy(Type.HTTP, new InetSocketAddress(proxyHost, Integer.valueOf(proxyPort)));
         }
-        return new Proxy(Type.HTTP, new InetSocketAddress("itproxy-dev.1and1.org", 3128));
+        return Proxy.NO_PROXY;
     }
 
     @Override
@@ -31,5 +51,10 @@ public class ProxyFactoryBean implements FactoryBean<Proxy> {
     @Override
     public boolean isSingleton() {
         return true;
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.env = applicationContext.getEnvironment();
     }
 }
