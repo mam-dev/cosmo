92e89aa5dcb6a617f95e7933d309f458d4302704
danielgrigore <danielgrigore@gmail.com>
2016-01-29 15:30:27 +0200

    Extracted UrlContentReader class for better separation.

--- cosmo-core/src/test/unit/java/org/unitedinternet/cosmo/ext/UrlContentReaderTest.java
+++ cosmo-core/src/test/unit/java/org/unitedinternet/cosmo/ext/UrlContentReaderTest.java
@@ -23,13 +23,13 @@ import org.unitedinternet.cosmo.model.hibernate.HibEntityFactory;
  * @author daniel grigore
  *
  */
-public class UrlContentSourceTest {
+public class UrlContentReaderTest {
 
     private ContentConverter converter;
 
     private LocalValidatorFactoryBean validator;
 
-    private UrlContentSource instanceUnderTest;
+    private UrlContentReader instanceUnderTest;
 
     @Before
     public void setUp() {
@@ -44,39 +44,40 @@ public class UrlContentSourceTest {
         EntityConverter entityConverter = new EntityConverter(entityFactory);
         this.converter = new ContentConverter(entityConverter);
 
-        instanceUnderTest = new UrlContentSource(converter, Proxy.NO_PROXY, validator, 700, 10000);
+        instanceUnderTest = new UrlContentReader(converter, Proxy.NO_PROXY, validator, 700);
     }
 
     @Test
     public void shouldReadLocalCalendar() {
-        Set<NoteItem> items = this.instanceUnderTest.getContent(urlForName("chandler-plain-event.ics"));
+        Set<NoteItem> items = this.instanceUnderTest.getContent(urlForName("chandler-plain-event.ics"), 0);
         assertNotNull(items);
         assertEquals(1, items.size());
     }
 
     @Test
     public void shouldReadRomanianHolidays() {
-        this.instanceUnderTest = new UrlContentSource(converter, Proxy.NO_PROXY, validator, 1024 * 1024, 10000);
-        Set<NoteItem> items = this.instanceUnderTest.getContent(urlForName("romanian-holidays.ics"));
+        this.instanceUnderTest = new UrlContentReader(converter, Proxy.NO_PROXY, validator, 1024 * 1024);
+        Set<NoteItem> items = this.instanceUnderTest.getContent(urlForName("romanian-holidays.ics"), 0);
         assertNotNull(items);
         assertEquals(80, items.size());
     }
 
     @Test(expected = InvalidExternalContentException.class)
     public void shouldFailAnInvalidEvent() {
-        instanceUnderTest.getContent(urlForName("invalid-event.ics"));
+        instanceUnderTest.getContent(urlForName("invalid-event.ics"), 0);
     }
 
     @Test(expected = ExternalContentTooLargeException.class)
     public void shouldFailTooLargeContent() {
-        instanceUnderTest.getContent(urlForName("2445.ics"));
+        instanceUnderTest.getContent(urlForName("2445.ics"), 0);
     }
 
     @Test
     @Ignore("Need only for testing purposes.")
     public void shouldReadExternalCalendar() {
         Set<NoteItem> items = this.instanceUnderTest.getContent(
-                "https://calendar.google.com/calendar/ical/8ojgn92qi1921h78j3n4p7va4s%40group.calendar.google.com/public/basic.ics");
+                "https://calendar.google.com/calendar/ical/8ojgn92qi1921h78j3n4p7va4s%40group.calendar.google.com/public/basic.ics",
+                0);
         assertNotNull(items);
         assertFalse(items.isEmpty());
         assertEquals(9, items.size());
