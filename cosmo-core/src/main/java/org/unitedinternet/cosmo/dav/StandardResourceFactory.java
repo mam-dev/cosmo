6bc2cce3ef39a543400362a8f4d768fc9b1a33f8
Daniel Grigore <daniel.grigore@gmail.com>
2019-03-20 11:14:57 +0200

    improvement: Do not allow regular file creation on home collection.

--- cosmo-core/src/main/java/org/unitedinternet/cosmo/dav/StandardResourceFactory.java
+++ cosmo-core/src/main/java/org/unitedinternet/cosmo/dav/StandardResourceFactory.java
@@ -66,10 +66,9 @@ import org.unitedinternet.cosmo.util.UriTemplate;
  */
 @Service
 @Transactional(readOnly = true)
-public class StandardResourceFactory
-    implements DavResourceFactory {
-    
-    private static final Log LOG =  LogFactory.getLog(StandardResourceFactory.class);
+public class StandardResourceFactory implements DavResourceFactory {
+
+    private static final Log LOG = LogFactory.getLog(StandardResourceFactory.class);
 
     private ContentService contentService;
     private UserService userService;
@@ -78,19 +77,15 @@ public class StandardResourceFactory
     private CalendarQueryProcessor calendarQueryProcessor;
     private ICalendarClientFilterManager clientFilterManager;
     private UserIdentitySupplier userIdentitySupplier;
-    
+
     private boolean schedulingEnabled = false;
 
-    public StandardResourceFactory(ContentService contentService,
-                                   UserService userService,
-                                   CosmoSecurityManager securityManager,
-                                   EntityFactory entityFactory,
-                                   CalendarQueryProcessor calendarQueryProcessor,
-                                   ICalendarClientFilterManager clientFilterManager,
-                                   UserIdentitySupplier userIdentitySupplier,
-                                   @Value("${cosmo.caldav.schedulingEnabled}") boolean schedulingEnabled) {
-    	
-    	
+    public StandardResourceFactory(ContentService contentService, UserService userService,
+            CosmoSecurityManager securityManager, EntityFactory entityFactory,
+            CalendarQueryProcessor calendarQueryProcessor, ICalendarClientFilterManager clientFilterManager,
+            UserIdentitySupplier userIdentitySupplier,
+            @Value("${cosmo.caldav.schedulingEnabled}") boolean schedulingEnabled) {
+
         this.contentService = contentService;
         this.userService = userService;
         this.securityManager = securityManager;
@@ -106,62 +101,54 @@ public class StandardResourceFactory
      * Resolves a {@link DavResourceLocator} into a {@link WebDavResource}.
      * </p>
      * <p>
-     * If the identified resource does not exist and the request method
-     * indicates that one is to be created, returns a resource backed by a 
-     * newly-instantiated item that has not been persisted or assigned a UID.
-     * Otherwise, if the resource does not exists, then a
-     * {@link NotFoundException} is thrown.
+     * If the identified resource does not exist and the request method indicates that one is to be created, returns a
+     * resource backed by a newly-instantiated item that has not been persisted or assigned a UID. Otherwise, if the
+     * resource does not exists, then a {@link NotFoundException} is thrown.
      * </p>
      * <p>
      * The type of resource to create is chosen as such:
      * <ul>
      * <li><code>MKCALENDAR</code>: {@link DavCalendarCollection}</li>
      * <li><code>MKCOL</code>: {@link DavCollectionBase}</li>
-     * <li><code>PUT</code>, <code>COPY</code>, <code>MOVE</code></li>:
-     * {@link DavFile}</li>
+     * <li><code>PUT</code>, <code>COPY</code>, <code>MOVE</code></li>: {@link DavFile}</li>
      * </ul>
      */
-    public WebDavResource resolve(DavResourceLocator locator,
-                               DavRequest request)
-        throws CosmoDavException {
-        WebDavResource resource = resolve(locator);
+    public WebDavResource resolve(DavResourceLocator locator, DavRequest request) throws CosmoDavException {
+        WebDavResource resource = this.resolve(locator);
         if (resource != null) {
             return resource;
         }
 
-        // we didn't find an item in storage for the resource, so either
-        // the request is creating a resource or the request is targeting a
-        // nonexistent item.
+        /*
+         * We didn't find an item in storage for the resource, so either the request is creating a resource or the
+         * request is targeting a nonexistent item.
+         */
         if (request.getMethod().equals("MKCALENDAR")) {
-            return new DavCalendarCollection(locator, this,entityFactory);
+            return new DavCalendarCollection(locator, this, entityFactory);
         }
         if (request.getMethod().equals("MKCOL")) {
             return new DavCollectionBase(locator, this, entityFactory);
         }
         if (request.getMethod().equals("PUT")) {
-            // will be replaced by the provider if a different resource
-            // type is required
+            // Will be replaced by the provider if a different resource type is required
             WebDavResource parent = resolve(locator.getParentLocator());
             if (parent instanceof DavCalendarCollection) {
                 return new DavEvent(locator, this, entityFactory);
             }
-            return new DavFile(locator, this, entityFactory);
         }
-        
-        // handle OPTIONS for non-existent resource
-        if(request.getMethod().equals("OPTIONS")) { 
-            // ensure parent exists first
+
+        // Handle OPTIONS for non-existent resource
+        if (request.getMethod().equals("OPTIONS")) {
+            // Ensure parent exists first
             WebDavResource parent = resolve(locator.getParentLocator());
-            if(parent!=null && parent.exists()) {
-                if(parent instanceof DavCalendarCollection) {
+            if (parent != null && parent.exists()) {
+                if (parent instanceof DavCalendarCollection) {
                     return new DavEvent(locator, this, entityFactory);
-                }
-                else {
+                } else {
                     return new DavCollectionBase(locator, this, entityFactory);
                 }
             }
         }
-    
         throw new NotFoundException();
     }
 
@@ -173,8 +160,7 @@ public class StandardResourceFactory
      * If the identified resource does not exists, returns <code>null</code>.
      * </p>
      */
-    public WebDavResource resolve(DavResourceLocator locator)
-        throws CosmoDavException {
+    public WebDavResource resolve(DavResourceLocator locator) throws CosmoDavException {
         String uri = locator.getPath();
         if (LOG.isDebugEnabled()) {
             LOG.debug("resolving URI " + uri);
@@ -202,12 +188,12 @@ public class StandardResourceFactory
             return createUserPrincipalResource(locator, match);
         }
 
-        if(schedulingEnabled) {
+        if (schedulingEnabled) {
             match = TEMPLATE_USER_INBOX.match(uri);
             if (match != null) {
                 return new DavInboxCollection(locator, this);
             }
-            
+
             match = TEMPLATE_USER_OUTBOX.match(uri);
             if (match != null) {
                 return new DavOutboxCollection(locator, this);
@@ -219,27 +205,22 @@ public class StandardResourceFactory
 
     /**
      * <p>
-     * Instantiates a <code>WebDavResource</code> representing the
-     * <code>Item</code> located by the given <code>DavResourceLocator</code>.
+     * Instantiates a <code>WebDavResource</code> representing the <code>Item</code> located by the given
+     * <code>DavResourceLocator</code>.
      * </p>
      */
-    public WebDavResource createResource(DavResourceLocator locator,
-                                      Item item)
-        throws CosmoDavException {
+    public WebDavResource createResource(DavResourceLocator locator, Item item) throws CosmoDavException {
         if (item == null) {
             throw new IllegalArgumentException("item cannot be null");
         }
         if (item instanceof HomeCollectionItem) {
-            return new DavHomeCollection((HomeCollectionItem) item, locator,
-                                         this, entityFactory);
+            return new DavHomeCollection((HomeCollectionItem) item, locator, this, entityFactory);
         }
 
         if (item instanceof CollectionItem) {
             if (item.getStamp(CalendarCollectionStamp.class) != null) {
-                return new DavCalendarCollection((CollectionItem) item,
-                                                 locator, this,entityFactory);
-            }
-            else {
+                return new DavCalendarCollection((CollectionItem) item, locator, this, entityFactory);
+            } else {
                 return new DavCollectionBase((CollectionItem) item, locator, this, entityFactory);
             }
         }
@@ -247,21 +228,19 @@ public class StandardResourceFactory
         if (item instanceof NoteItem) {
             NoteItem note = (NoteItem) item;
             // don't expose modifications
-            if(note.getModifies()!=null) {
+            if (note.getModifies() != null) {
                 return null;
-            }
-            else if (item.getStamp(EventStamp.class) != null) {
+            } else if (item.getStamp(EventStamp.class) != null) {
                 return new DavEvent(note, locator, this, entityFactory);
-            }
-            else {
+            } else {
                 return new DavTask(note, locator, this, entityFactory);
             }
         }
-        
-        if(item instanceof FreeBusyItem) {
+
+        if (item instanceof FreeBusyItem) {
             return new DavFreeBusy((FreeBusyItem) item, locator, this, entityFactory);
         }
-        if(item instanceof AvailabilityItem) {
+        if (item instanceof AvailabilityItem) {
             return new DavAvailability((AvailabilityItem) item, locator, this, entityFactory);
         }
 
@@ -270,29 +249,21 @@ public class StandardResourceFactory
 
     // our methods
 
-    protected WebDavResource createUidResource(DavResourceLocator locator,
-                                            UriTemplate.Match match)
-        throws CosmoDavException {
+    protected WebDavResource createUidResource(DavResourceLocator locator, UriTemplate.Match match)
+            throws CosmoDavException {
         String uid = match.get("uid");
         String path = match.get("*");
-        Item item = path != null ?
-            contentService.findItemByPath(path, uid) :
-            contentService.findItemByUid(uid);
+        Item item = path != null ? contentService.findItemByPath(path, uid) : contentService.findItemByUid(uid);
         return item != null ? createResource(locator, item) : null;
     }
 
-    protected WebDavResource
-        createUserPrincipalResource(DavResourceLocator locator,
-                                    UriTemplate.Match match)
-        throws CosmoDavException {
+    protected WebDavResource createUserPrincipalResource(DavResourceLocator locator, UriTemplate.Match match)
+            throws CosmoDavException {
         User user = userService.getUser(match.get("username"));
-        return user != null ? new DavUserPrincipal(user, locator, this, userIdentitySupplier) :
-            null;
+        return user != null ? new DavUserPrincipal(user, locator, this, userIdentitySupplier) : null;
     }
 
-    protected WebDavResource createUnknownResource(DavResourceLocator locator,
-                                                String uri)
-        throws CosmoDavException {
+    protected WebDavResource createUnknownResource(DavResourceLocator locator, String uri) throws CosmoDavException {
         Item item = contentService.findItemByPath(uri);
         return item != null ? createResource(locator, item) : null;
     }
@@ -300,7 +271,7 @@ public class StandardResourceFactory
     public ContentService getContentService() {
         return contentService;
     }
-    
+
     public CalendarQueryProcessor getCalendarQueryProcessor() {
         return calendarQueryProcessor;
     }
