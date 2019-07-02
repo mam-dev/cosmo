0ad5f5016da1b911cc5503ea02b00d46da59b35a
Daniel Grigore <daniel.grigore@gmail.com>
2019-07-02 14:37:53 +0300

    updated echo messages.

---
+++ .travis/maven-deploy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "Setting new version for release to $TRAVIS_TAG"
+mvn org.codehaus.mojo:versions-maven-plugin:set -DnewVersion=$TRAVIS_TAG
+
+echo "Start deploying to https://oss.sonatype.org/#stagingRepositories"
+mvn deploy --settings .travis/settings.xml -DskipTests=true --batch-mode --update-snapshots
--- .travis/maven-release.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-echo "Ensuring that pom  matches $TRAVIS_TAG"
-mvn org.codehaus.mojo:versions-maven-plugin:set -DnewVersion=$TRAVIS_TAG
-
-echo "Uploading to oss repo "
-mvn deploy --settings .travis/settings.xml -DskipTests=true --batch-mode --update-snapshots
