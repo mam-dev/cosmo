4c4b8da206034adb440dc7b6cb8fd864f3329676
Daniel Grigore <daniel.grigore@gmail.com>
2019-07-03 17:32:44 +0300

    perform clean deploy.

--- .travis/maven-deploy.sh
+++ .travis/maven-deploy.sh
@@ -6,4 +6,4 @@ echo "Setting new version for release to $TRAVIS_TAG"
 mvn org.codehaus.mojo:versions-maven-plugin:set -DnewVersion=$TRAVIS_TAG
 
 echo "Start deploying to https://oss.sonatype.org/#stagingRepositories"
-mvn deploy --settings .travis/settings.xml -DskipTests=true -DskipLocalStaging=true --batch-mode
+mvn clean deploy --settings .travis/settings.xml -DskipTests=true --batch-mode
