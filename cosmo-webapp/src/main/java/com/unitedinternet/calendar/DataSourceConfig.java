69e7dc33956d0cd74d4d7f91b3c8c3ac79878005
Pavel Zinin <pzinin@gmail.com>
2019-10-18 18:37:27 +0300

    Set embedded MariaDB as default DS

--- cosmo-webapp/src/main/java/com/unitedinternet/calendar/DataSourceConfig.java
+++ cosmo-webapp/src/main/java/com/unitedinternet/calendar/DataSourceConfig.java
@@ -2,6 +2,7 @@ package com.unitedinternet.calendar;
 
 import javax.sql.DataSource;
 
+import ch.vorburger.mariadb4j.springframework.MariaDB4jSpringService;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,11 +23,24 @@ import java.util.Properties;
 @Configuration
 @ConfigurationProperties("spring.datasource")
 public class DataSourceConfig extends HikariConfig {
+
+    @Bean(initMethod = "start", destroyMethod = "stop", name = "mariaDB")
+    public MariaDB4jSpringService mariaDBService() {
+        MariaDB4jSpringService db = new MariaDB4jSpringService();
+        db.setDefaultBaseDir("target/maridb/base");
+        db.setDefaultDataDir("target/maridb/data");
+        db.setDefaultPort(33060);
+        return db;
+    }
+
     @Bean
+    @DependsOn("mariaDB")
     public DataSource ds() {
-        this.setJdbcUrl("jdbc:mysql://localhost:3306/caldav");
-        this.setUsername("test");
-        this.setPassword("test");
-        return  new HikariDataSource(this);
+        this.setJdbcUrl("jdbc:mysql://localhost:33060/test?autoReconnect=true");
+        this.setUsername("root");
+        this.setPassword("");
+        this.setDriverClassName("org.mariadb.jdbc.Driver");
+        return new HikariDataSource(this);
     }
 }
+
